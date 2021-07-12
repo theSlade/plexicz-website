ec7d5e4e854f7d23cb49e2ed49b55633d289eb55
DevLasseV <77295879+DevLasseV@users.noreply.github.com>
2021-07-12 04:06:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from '../src/components/Images'
 import HomePage from '../src/pages/HomePage'
 import AboutPage from '../src/pages/AboutPage'
 import TeamPage from '../src/pages/TeamPage'
@@ -10,7 +9,6 @@ function App() {
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path="/image" component={Image} />
           <Route path="/team" component={TeamPage} />
           <Route path="/about" component={AboutPage} />
           <Route path="/" component={HomePage} />
